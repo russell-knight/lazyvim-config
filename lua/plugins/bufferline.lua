2442bf915653e8f4136c980b1154f11481a6dead
Russell Knight <13390184+russell-knight@users.noreply.github.com>
2026-01-03 11:16:04 +1100

    Disable bufferline plugin

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,5 @@
+-- Disable bufferline.nvim to remove buffer tabs
+-- Use <leader>fb (Telescope buffer picker) for buffer navigation instead
+return {
+  { "akinsho/bufferline.nvim", enabled = false },
+}
