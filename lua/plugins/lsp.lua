009f53dcde948bd1ce1bde01ab5f46fb915b8751
Russell Knight <13390184+russell-knight@users.noreply.github.com>
2026-02-27 17:02:56 +1100

    feat: use tsgo instead of vtsls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,6 +3,8 @@ return {
   opts = {
     servers = {
       omnisharp = false, -- disable omnisharp
+      vtsls = false, -- disable vtsls in favor of tsgo
+      tsgo = {}, -- enable tsgo TypeScript language server
     },
   },
 }
