5fbea5b18e58ed061214ec73bd1d1a403918a58e
Russell Knight <13390184+russell-knight@users.noreply.github.com>
2026-02-27 17:02:36 +1100

    refactor: move disabled bufferline to disabled.lua file and also disable snacks_explorer

--- lua/plugins/bufferline.lua
+++
@@ -1,5 +0,0 @@
--- Disable bufferline.nvim to remove buffer tabs
--- Use <leader>fb (Telescope buffer picker) for buffer navigation instead
-return {
-  { "akinsho/bufferline.nvim", enabled = false },
-}
---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,5 @@
+return {
+  { "akinsho/bufferline.nvim", enabled = false },
+  -- Disable Snacks Explorer (using mini.files instead)
+  { import = "lazyvim.plugins.extras.editor.snacks_explorer", enabled = false },
+}
